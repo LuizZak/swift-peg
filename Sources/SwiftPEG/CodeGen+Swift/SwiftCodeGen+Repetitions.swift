e3331d4d30a3faf80ba2550aabc4bf9b3613d27d
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-06-03 17:16:41 -0300

    Adding code generation support for one-or-more minimal/maximal repetitions

--- Sources/SwiftPEG/CodeGen+Swift/SwiftCodeGen+Repetitions.swift
+++ Sources/SwiftPEG/CodeGen+Swift/SwiftCodeGen+Repetitions.swift
@@ -27,10 +27,9 @@ extension SwiftCodeGen {
             buffer.ensureDoubleNewline()
             buffer.emitLine("// Collect an extra item and try again")
             buffer.emitLine("if")
+            declContext.push()
             var nextBindings: [String] = []
             try buffer.indented {
-                declContext.push()
-                defer { declContext.pop() }
                 let item = InternalGrammar.Item.atom(info.repetitionAtom)
                 let bindings = bindings(for: info.repetitionAtom).scg_unwrapped()
 
@@ -47,6 +46,8 @@ extension SwiftCodeGen {
                 let expr = defaultReturnExpression(for: nextBindings.scgr_asTupleExpr())
 
                 buffer.emitLine("\(currentArray).append(\(expr))")
+
+                declContext.pop()
             }
             buffer.emitBlock(" else ") {
                 buffer.emit("break")
@@ -99,7 +100,7 @@ extension SwiftCodeGen {
             declContext.push()
             defer { declContext.pop() }
 
-            buffer.emitLine("let _endMark = _current.last?.0 ?? self.mark()")
+            buffer.emitLine("let _endMark = \(currentArray).last?.0 ?? self.mark()")
             buffer.emitLine("self.restore(_endMark)")
 
             // if let <trail> = <trail>()
@@ -122,12 +123,110 @@ extension SwiftCodeGen {
 
     /// Produces a one-or-more minimal (`+<`) repetition parsing method.
     func generateOneOrMoreMinimalBody(_ info: RepetitionBodyGenInfo) throws {
+        let currentArray = "_current"
+        let repetitionItemType = info.repetitionAtomType.scg_unwrapped().scg_asValidSwiftType()
 
+        buffer.emitLine("var \(currentArray): [\(repetitionItemType)] = []")
+
+        buffer.ensureDoubleNewline()
+        buffer.emitLine("while")
+        declContext.push()
+
+        var nextBindings: [String] = []
+        try buffer.indented {
+            let item = InternalGrammar.Item.atom(info.repetitionAtom)
+            let bindings = bindings(for: info.repetitionAtom).scg_unwrapped()
+
+            nextBindings.append(contentsOf:
+                try generateBindingsToItem(
+                    item,
+                    bindings,
+                    in: info.production
+                )
+            )
+        }
+
+        buffer.ensureNewline()
+        try buffer.emitBlock {
+            defer { declContext.pop() }
+
+            let expr = defaultReturnExpression(for: nextBindings.scgr_asTupleExpr())
+            buffer.emitLine("\(currentArray).append(\(expr))")
+            buffer.emitLine("let _mark = self.mark()")
+
+            // if let <trail> = <trail>()
+            //   ...
+            buffer.ensureDoubleNewline()
+            try _generateSuccessIfLet(info, leftExpression: currentArray)
+
+            buffer.ensureDoubleNewline()
+            buffer.emitLine("self.restore(_mark)")
+        }
+
+        buffer.ensureDoubleNewline()
+        buffer.emit("return \(info.failReturnExpression)")
     }
 
     /// Produces a one-or-more maximal (`+>`) repetition parsing method.
     func generateOneOrMoreMaximalBody(_ info: RepetitionBodyGenInfo) throws {
+        let currentArray = "_current"
+        let repetitionItemType = info.repetitionAtomType.scg_unwrapped().scgr_flattened()
+        let arrayElementType = CommonAbstract.SwiftType.tuple([
+            .unlabeled("Mark"),
+            .unlabeled(repetitionItemType),
+        ])
 
+        // Initial element capturing
+
+        buffer.emitLine("// Start by fetching as many productions as possible")
+        buffer.emitLine("guard")
+        try buffer.indented {
+            buffer.emit("var \(currentArray): [\(arrayElementType)] = ")
+            buffer.emit("try self.repeatZeroOrMore(")
+            try buffer.emitInlinedBlock {
+                declContext.push()
+                defer { declContext.pop() }
+
+                let item = InternalGrammar.Item.atom(info.repetitionAtom)
+                let bindings = bindings(for: info.repetitionAtom).scg_unwrapped()
+
+                buffer.emit("if ")
+                let bound = try generateBindingsToItem(item, bindings, in: info.production)
+                let mapTupleExpr = (["self.mark()"] + bound).scgr_asTupleExpr()
+                buffer.emitLine(" { return \(self.defaultReturnExpression(for: mapTupleExpr)) }")
+                buffer.emitLine("return nil")
+            }
+            buffer.emitLine(")")
+        }
+        buffer.emitBlock("else ") {
+            buffer.emitLine("return \(info.failReturnExpression)")
+        }
+
+        // Main while loop
+
+        buffer.ensureDoubleNewline()
+        try buffer.emitBlock("while let _end = \(currentArray).last") {
+            declContext.push()
+            defer { declContext.pop() }
+
+            buffer.emitLine("self.restore(_end.0)")
+
+            // if let <trail> = <trail>()
+            //   ...
+            buffer.ensureDoubleNewline()
+            try _generateSuccessIfLet(info, leftExpression: "\(currentArray).map(\\.1)")
+            buffer.backtrackWhitespace()
+            buffer.emitBlock(" else if \(currentArray).count <= 1 ") {
+                buffer.emit("return \(info.failReturnExpression)")
+            }
+
+            buffer.ensureDoubleNewline()
+            buffer.emitLine("// Drop an item, backtrack the parser, and try again")
+            buffer.emitLine("\(currentArray).removeLast()")
+        }
+
+        buffer.ensureDoubleNewline()
+        buffer.emit("return \(info.failReturnExpression)")
     }
 
     /// Generates the if-let binding that succeeds the associated repeated
--- Tests/SwiftPEGTests/CodeGen+Swift/SwiftCodeGen+RepetitionsTests.swift
+++ Tests/SwiftPEGTests/CodeGen+Swift/SwiftCodeGen+RepetitionsTests.swift
@@ -319,6 +319,315 @@ class SwiftCodeGen_RepetitionsTests: XCTestCase {
             }
             """#).diff(result)
     }
+    func testGenerateParser_oneOrMore_minimal() throws {
+        let grammar = try parseGrammar("""
+        @token d; @token e; @tokenCallKind "expectKind" ;
+        start: a b+< c;
+        a[A]: 'a' ;
+        b[B]: 'b' ;
+        c[C]: 'c'? 'e' ;
+        """)
+        let sut = makeSut(grammar)
+
+        let result = try sut.generateParser(
+            settings: .default.with(\.emitTypesInBindings, value: true)
+        )
+
+        diffTest(expected: """
+            extension Parser {
+                /// ```
+                /// start:
+                ///     | a b+< c
+                ///     ;
+                /// ```
+                @memoized("start")
+                @inlinable
+                public func __start() throws -> Node? {
+                    let mark = self.mark()
+
+                    if
+                        let a: A = try self.a(),
+                        case let (b?, c?) = try self._start__nsr()
+                    {
+                        return Node()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// a[A]:
+                ///     | 'a'
+                ///     ;
+                /// ```
+                @memoized("a")
+                @inlinable
+                public func __a() throws -> A? {
+                    let mark = self.mark()
+
+                    if
+                        let _: TokenResult = try self.expect("a")
+                    {
+                        return A()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// b[B]:
+                ///     | 'b'
+                ///     ;
+                /// ```
+                @memoized("b")
+                @inlinable
+                public func __b() throws -> B? {
+                    let mark = self.mark()
+
+                    if
+                        let _: TokenResult = try self.expect("b")
+                    {
+                        return B()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// c[C]:
+                ///     | 'c'? 'e'
+                ///     ;
+                /// ```
+                @memoized("c")
+                @inlinable
+                public func __c() throws -> C? {
+                    let mark = self.mark()
+
+                    if
+                        let _: TokenResult? = try self.optional({
+                            try self.expect("c")
+                        }),
+                        let _: TokenResult = try self.expect("e")
+                    {
+                        return C()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// _start__nsr[(b: [B], c: C)]:
+                ///     | b+< _start__nsr_tail
+                ///     ;
+                /// ```
+                @memoized("_start__nsr")
+                @inlinable
+                public func ___start__nsr() throws -> (b: [B]?, c: C?) {
+                    var _current: [B] = []
+
+                    while
+                        let b: B = try self.b()
+                    {
+                        _current.append(b)
+                        let _mark = self.mark()
+
+                        if
+                            let c: C = try self._start__nsr_tail()
+                        {
+                            return (_current, c)
+                        }
+
+                        self.restore(_mark)
+                    }
+
+                    return (nil, nil)
+                }
+
+                /// ```
+                /// _start__nsr_tail[C]:
+                ///     | c
+                ///     ;
+                /// ```
+                @memoized("_start__nsr_tail")
+                @inlinable
+                public func ___start__nsr_tail() throws -> C? {
+                    let mark = self.mark()
+
+                    if
+                        let c: C = try self.c()
+                    {
+                        return c
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+            }
+            """).diff(result)
+    }
+
+    func testGenerateParser_oneOrMore_maximal() throws {
+        let grammar = try parseGrammar("""
+        @token d; @token e; @tokenCallKind "expectKind" ;
+        start: a b+> c;
+        a[A]: 'a' ;
+        b[B]: 'b' ;
+        c[C]: 'c'? 'e' ;
+        """)
+        let sut = makeSut(grammar)
+
+        let result = try sut.generateParser(
+            settings: .default.with(\.emitTypesInBindings, value: true)
+        )
+
+        diffTest(expected: #"""
+            extension Parser {
+                /// ```
+                /// start:
+                ///     | a b+> c
+                ///     ;
+                /// ```
+                @memoized("start")
+                @inlinable
+                public func __start() throws -> Node? {
+                    let mark = self.mark()
+
+                    if
+                        let a: A = try self.a(),
+                        case let (b?, c?) = try self._start__nsr()
+                    {
+                        return Node()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// a[A]:
+                ///     | 'a'
+                ///     ;
+                /// ```
+                @memoized("a")
+                @inlinable
+                public func __a() throws -> A? {
+                    let mark = self.mark()
+
+                    if
+                        let _: TokenResult = try self.expect("a")
+                    {
+                        return A()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// b[B]:
+                ///     | 'b'
+                ///     ;
+                /// ```
+                @memoized("b")
+                @inlinable
+                public func __b() throws -> B? {
+                    let mark = self.mark()
+
+                    if
+                        let _: TokenResult = try self.expect("b")
+                    {
+                        return B()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// c[C]:
+                ///     | 'c'? 'e'
+                ///     ;
+                /// ```
+                @memoized("c")
+                @inlinable
+                public func __c() throws -> C? {
+                    let mark = self.mark()
+
+                    if
+                        let _: TokenResult? = try self.optional({
+                            try self.expect("c")
+                        }),
+                        let _: TokenResult = try self.expect("e")
+                    {
+                        return C()
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+
+                /// ```
+                /// _start__nsr[(b: [B], c: C)]:
+                ///     | b+> _start__nsr_tail
+                ///     ;
+                /// ```
+                @memoized("_start__nsr")
+                @inlinable
+                public func ___start__nsr() throws -> (b: [B]?, c: C?) {
+                    // Start by fetching as many productions as possible
+                    guard
+                        var _current: [(Mark, B)] = try self.repeatZeroOrMore({
+                            if let b: B = try self.b() { return (self.mark(), b) }
+                            return nil
+                        })
+                    else {
+                        return (nil, nil)
+                    }
+
+                    while let _end = _current.last {
+                        self.restore(_end.0)
+
+                        if
+                            let c: C = try self._start__nsr_tail()
+                        {
+                            return (_current.map(\.1), c)
+                        } else if _current.count <= 1 {
+                            return (nil, nil)
+                        }
+
+                        // Drop an item, backtrack the parser, and try again
+                        _current.removeLast()
+                    }
+
+                    return (nil, nil)
+                }
+
+                /// ```
+                /// _start__nsr_tail[C]:
+                ///     | c
+                ///     ;
+                /// ```
+                @memoized("_start__nsr_tail")
+                @inlinable
+                public func ___start__nsr_tail() throws -> C? {
+                    let mark = self.mark()
+
+                    if
+                        let c: C = try self.c()
+                    {
+                        return c
+                    }
+
+                    self.restore(mark)
+                    return nil
+                }
+            }
+            """#).diff(result)
+    }
 }
 
 // MARK: - Test internals
